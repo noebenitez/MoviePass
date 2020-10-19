07c5d7ed1aab25a0f75c9beca8854f8c8b8ed07e
Cande Yarossi <63622855+candyarossi@users.noreply.github.com>
2020-10-19 15:01:10 -0300

    Prueba x

--- config/constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define( 'ROOT', dirname(__DIR__) );
-
-define( 'PELICULAS', 'https://api.themoviedb.org/3/movie/now_playing?api_key=f20416aa14acdc6b2cd1af3feb7633a6&language=es&region=AR' );
-
-define( 'GENEROS', 'https://api.themoviedb.org/3/genre/movie/list?api_key=f20416aa14acdc6b2cd1af3feb7633a6&language=es' );
